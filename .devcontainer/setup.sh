66f3263a33bf1ab4671d24ab149d907572c2d550
Michael Friderich <michael@frideri.ch>
2025-12-12 15:33:44 +0000

    update devcontainer config and setup script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,17 +3,3 @@
 echo "running setup script"
 
 apt-get update
-
-# Install ZSH
-apt-get install -y zsh
-
-# Install dotfiles
-# if [ -d "$HOME/.dotfiles" ]; then
-#   cd $HOME/.dotfiles
-#   ./install.sh || ./setup.sh || echo "No install script found, manual setup may be required."
-# else
-#   echo "Dotfiles directory not found. Ensure it's mounted correctly."
-# fi
-
-# Set ZSH as default shell
-# chsh -s $(which zsh)
